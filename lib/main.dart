bf1abc6c0e817296a78011bf4155cfac74f9ad9d
fynnj <fynnjuranek@web.de>
2023-08-09 18:02:42 +0200

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
-import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +17,8 @@ void main() {
       home: const HomePage(),
       routes: {
         "/login/": (context) => const LoginView(),
-        "/register/": (context) => const RegisterView()
+        "/register/": (context) => const RegisterView(),
+        "/notes/": (context) => const NotesView(),
       }));
 }
 
@@ -37,7 +37,6 @@ class HomePage extends StatelessWidget {
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                print("Email is verified");
                 return const NotesView();
               } else {
                 return const VerifyEmailView();
